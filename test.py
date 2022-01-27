d39db31f8a483e9daad63680ca180b45856b6254
CalmaQi <43681070+CalmaQi@users.noreply.github.com>
2022-01-27 11:29:00 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+import requests
+import numpy
+import time
+time1=time.localtime( time.time() )
+mytime=str(time1[0])+'-'+str(time1[1])+'-'+str(time1[2])+' '
+mytime+=str(time1[3])+':'+str(time1[4])+':'+str(time1[5])
